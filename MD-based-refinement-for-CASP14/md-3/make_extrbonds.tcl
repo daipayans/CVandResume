252606a33f657956f753a45d653e626edd0986e9
Daipayan Sarkar <sdaipayan@gmail.com>
2022-08-03 02:56:15 -0400

    Delete MD-based-refinement-for-CASP14 directory

--- MD-based-refinement-for-CASP14/md-3/make_extrbonds.tcl
+++
@@ -1,8 +0,0 @@
-package require ssrestraints
-
-mol new ionized.hmr.psf
-mol addfile ionized.pdb
-
-ssrestraints -psf ionized.hmr.psf -pdb ionized.pdb -o ss-extrabonds.txt -hbonds
-
-quit
--- MD-based-refinement-for-CASP14/system/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-infile="$1"
-module load vmd/1.9.3
-vmd -dispdev text -e genpsfpdb.tcl -args ${infile} 
-vmd -dispdev text -e hmr.tcl -args ionized.psf ionized.pdb
